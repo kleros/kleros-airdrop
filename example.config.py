a92bebdeeb8dd2b4c0996480153c5f4b24f3705e
Sam Vitello <sam.j.vitello@gmail.com>
2018-04-05 18:25:03 -0700

    update example config

--- example.config.py
+++ example.config.py
@@ -1,6 +1,7 @@
 config = {
-    ETH_PROVIDER: "https://kovan.infura.io/",
-    AIRDROPPER_CONTRACT_ADDRESS: "",
-    PRIVATE_KEY: "",
-    BATCH_SIZE: 100
+    "ETH_PROVIDER": "https://kovan.infura.io/",
+    "AIRDROPPER_CONTRACT_ADDRESS": "",
+    "PRIVATE_KEY": "",
+    "BATCH_SIZE": 100,
+    "GAS": 440000
 }
